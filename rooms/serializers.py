ec44de50e91362b835c001c827f04be9cc9f2b57
himzei <himzei@gmail.com>
2020-07-29 14:50:18 +0900

    api rest

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,12 +1,13 @@
 
 from rest_framework import serializers
-from users.serializers import RelatedUserSerializer
+from users.serializers import UserSerializer
 from .models import Room
 
 
 class RoomSerializer(serializers.ModelSerializer):
 
-    user = RelatedUserSerializer()
+    user = UserSerializer(read_only=True)
+    is_fav = serializers.SerializerMethodField()
 
     class Meta:
         model = Room
@@ -24,3 +25,16 @@ class RoomSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(
                 "Not enough time between changes")
         return data
+
+    def get_is_fav(self, obj):
+        request = self.context.get('request')
+        if request:
+            user = request.user
+            if user.is_authenticated:
+                return obj in user.favs.all()
+        return False
+
+    def create(self, validated_data):
+        request = self.context.get("request")
+        room = Room.objects.create(**validated_data, user=request.user)
+        return room
--- rooms/urls.py
+++ rooms/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from rest_framework.routers import DefaultRouter
 from . import views
 
-app_name = "rooms"
 
-urlpatterns = [
-    path("", views.RoomsView.as_view()),
-    path("<int:pk>/", views.RoomView.as_view()),
-]
+app_name = "rooms"
+router = DefaultRouter()
+router.register("", views.RoomViewSet)
+
+urlpatterns = router.urls
